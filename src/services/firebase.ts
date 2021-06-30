2639acfde5d39fc65c09680aaced9bc51aab4957
LeoCPeres <leonacsik@gmail.com>
2021-06-30 00:27:50 -0300

    Acesso

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,13 +4,13 @@ import "firebase/auth";
 import "firebase/database";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyDUJ5kOs0BJhdgH6M8DGcd6QzVHkrmWDVY",
+  authDomain: "letmeask-3a91b.firebaseapp.com",
+  databaseURL: "https://letmeask-3a91b-default-rtdb.firebaseio.com",
+  projectId: "letmeask-3a91b",
+  storageBucket: "letmeask-3a91b.appspot.com",
+  messagingSenderId: "466227364074",
+  appId: "1:466227364074:web:e83ba6a3e91bb482df1793",
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -18,4 +18,4 @@ firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const database = firebase.database();
 
-export { firebase, auth, database }
+export { firebase, auth, database };
